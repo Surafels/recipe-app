e98354ddadbd549f4e8e19f426fe82137b59b675
Ssurafels <ssurafel4@gmail.com>
2024-01-22 17:40:32 +0300

    Generate Food model,controler,views and update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_22_142658) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_22_143747) do
+  create_table "foods", force: :cascade do |t|
+    t.string "name"
+    t.string "measurment_unit"
+    t.decimal "price"
+    t.integer "quantity"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "foods", "users"
 end
