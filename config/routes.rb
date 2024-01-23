8ab5fa92657fb7add8077490c76934223326bf04
Noel Bryant <noelsobryant@gmail.com>
2024-01-24 00:49:59 +0300

    set public recipes as root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'public_recipes/index'
   devise_for :users
   resources :recipe_foods
   resources :recipes
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "public_recipes#index"
 end
