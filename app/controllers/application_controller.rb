c5dba7036c27a46c1b8086872d3f3c8e99f58779
Ssurafels <ssurafel4@gmail.com>
2024-01-23 15:52:51 +0300

    Update models,views and schema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+protect_from_forgery with: :exception
+before_action :authenticate_user!
+before_action :update_allowed_parameters, if: :devise_controller?
+
+def after_sign_in_path_for(resource)
+  flash[:notice] = "Successfully logged in. Welcome, #{resource.name}!"
+  stored_location_for(resource) || users_path
+end
+
+protected
+
+def update_allowed_parameters
+  devise_parameter_sanitizer.permit(:sign_up) do |u|
+    u.permit(:name, :email,  :password, :password_confirmation)
+  end
+  devise_parameter_sanitizer.permit(:account_update) do |u|
+    u.permit(:name, :email,  :password, :current_password)
+  end
+end
 end
--- app/models/food.rb
+++ app/models/food.rb
@@ -1,3 +1,10 @@
 class Food < ApplicationRecord
+  has_many :recipe_foods
   belongs_to :user
+
+  
+  validates :name, presence: true
+  validates :preparation_time, presence: true
+  validates :cooking_time, presence: true
+  validates :description, presence: true
 end
