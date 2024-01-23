838c39e64e1f79053cf28e93470e8cd2d562172d
Noel Bryant <noelsobryant@gmail.com>
2024-01-24 00:44:50 +0300

    remove authentication as the initial page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
-    before_action :authenticate_user!
+    # before_action :authenticate_user!
+
+    private
+
+    def redirect_to_public_recipes
+        redirect_to public_recipes_index_path if request.path == root_path
+    end
 end
