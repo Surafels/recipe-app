2bf12d93315cd85869c6f79b69f3f7567c3dc26b
Ssurafels <ssurafel4@gmail.com>
2024-01-23 18:07:35 +0300

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,21 +1,17 @@
 class ApplicationController < ActionController::Base
-protect_from_forgery with: :exception
-before_action :authenticate_user!
-before_action :update_allowed_parameters, if: :devise_controller?
+    # before_action :authenticate_user!
+    protect_from_forgery with: :exception
+  
+    before_action :update_allowed_parameters, if: :devise_controller?
 
-def after_sign_in_path_for(resource)
-  flash[:notice] = "Successfully logged in. Welcome, #{resource.name}!"
-  stored_location_for(resource) || users_path
-end
-
-protected
-
-def update_allowed_parameters
-  devise_parameter_sanitizer.permit(:sign_up) do |u|
-    u.permit(:name, :email,  :password, :password_confirmation)
+    # def after_sign_in_path_for(resource)
+    #     stored_location_for(resource) || users_path
+    #   end
+    
+    protected
+  
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
+    end
   end
-  devise_parameter_sanitizer.permit(:account_update) do |u|
-    u.permit(:name, :email,  :password, :current_password)
-  end
-end
-end
