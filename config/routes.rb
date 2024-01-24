8b6c167d27d52cf397e7cb32689e6bc62271029f
Noel Bryant <noelsobryant@gmail.com>
2024-01-25 00:01:22 +0300

    add custom controllers for devise registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'public_recipes/index'
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'registrations' }
   resources :users, except: :show
   get 'users/:id', to: 'users#show', as: :user_show, constraints: { id: /\d+/ }
   resources :recipe_foods
