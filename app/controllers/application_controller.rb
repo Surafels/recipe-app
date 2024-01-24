0f5cec2f282679cc46487886fd33529b7fee5ae5
Noel Bryant <noelsobryant@gmail.com>
2024-01-24 12:29:50 +0300

    fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
-    # before_action :authenticate_user!
+  # before_action :authenticate_user!
 
-    private
+  private
 
-    def redirect_to_public_recipes
-        redirect_to public_recipes_index_path if request.path == root_path
-    end
+  def redirect_to_public_recipes
+    redirect_to public_recipes_index_path if request.path == root_path
+  end
 end
