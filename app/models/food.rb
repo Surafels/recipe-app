96ecb96697be991bbe3a26da29a2de8c47af5362
Surafel Samson <ssurafel4@gmail.com>
2024-01-24 12:21:43 +0300

    Update food.rb

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,7 +2,6 @@ class Food < ApplicationRecord
   has_many :recipe_foods
   belongs_to :user
 
-
   validates :name, presence: true
   validates :preparation_time, presence: true
   validates :cooking_time, presence: true
